2595daaca68bf773895aea6a3e6e7243267434c0
Alex-Skripnik <Skripnik.O.Olex@gmail.com>
2022-10-09 14:30:49 +0300

    added comments to the program code

--- app.rb
+++ app.rb
@@ -8,12 +8,18 @@ def init_db
   @db.results_as_hash = true
 end
 
+# before вызывается каждый раз при перезагрузке любой страницы
 before do
+  # инициализация БД
   init_db
 end
 
+# configure вызывается каждый раз при конфигурации приложения:
+# когда изменился код программы и перегрузилась страница
 configure do
+  # инициализация БД
   init_db
+  # создаём таблицу если таблица не существует
   @db.execute 'create table if not exists "Pasts"
   (
     "id" INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -26,11 +32,14 @@ get '/' do
   erb 'Can you handle a <a href="/secure/place">secret</a>?'
 end
 
+# обработчик get-запроса /new (браузер получает страницу с сервера)
 get '/new' do
   erb :new
 end
 
+# обработчик post-запроса /new (браузер отправляет данные на сервера)
 post '/new' do
+  # получаем переменную из post-запроса
   content = params[:content]
   erb "You typed: #{content}"
 end
