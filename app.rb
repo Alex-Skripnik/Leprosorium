49be005acb194e64057fb3a0f9d06ba43b30896e
Alex-Skripnik <Skripnik.O.Olex@gmail.com>
2022-10-09 13:11:27 +0300

    Added database Leprosorium.db.

--- app.rb
+++ app.rb
@@ -1,6 +1,16 @@
 require 'rubygems'
 require 'sinatra'
 require 'sinatra/reloader'
+require 'sqlite3'
+
+def init_db
+  @db = SQLite3::Database.new 'Leprosorium.db'
+  @db.results_as_hash = true
+end
+
+before do
+  init_db
+end
 
 get '/' do
   erb 'Can you handle a <a href="/secure/place">secret</a>?'
@@ -8,4 +18,9 @@ end
 
 get '/new' do
   erb :new
+end
+
+post '/new' do
+  content = params[:content]
+  erb "You typed: #{content}"
 end
